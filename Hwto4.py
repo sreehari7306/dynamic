b6428e857abea7440f05c6c3657d09c91dd04c2e
SREEHARI P <sreeharisreehari682@gmail.com>
2024-10-15 21:48:25 +0530

    Hii

---
+++ Hwto4.py
@@ -0,0 +1,13 @@
+num=5
+for i in range (1,num+1):
+  if i<=3:
+    for j in range (1,i+1):
+      print(j,end="")
+  else:
+    for j in range (1,num-i+2):
+      print(j,end="")
+  print()
+      
+      
+      
+  
